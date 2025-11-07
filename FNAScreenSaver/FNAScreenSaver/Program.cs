792a6e2a00c223c9f4d041a8a130671cfcdf5078
ksenomin <ksenomin@mail.ru>
2025-11-07 10:39:52 +0300

    готово

--- FNAScreenSaver/FNAScreenSaver/Program.cs
+++ FNAScreenSaver/FNAScreenSaver/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using FNAScreenSaver.Core;
+
+namespace FNAScreenSaver
+{
+    /// <summary>
+    /// Класс запуска программы
+    /// </summary>
+    static class Program
+    {
+        /// <summary>
+        /// Точка входа программы
+        /// </summary>
+        public static void Main(string[] args)
+        {
+            using (var screenSaver = new GameEngine())
+            {
+                screenSaver.Run();
+            }
+        }
+    }
+
+}
